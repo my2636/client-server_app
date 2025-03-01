80ff74e407314ce88a1b846dd6258a99c49f34d0
m.y <myakk015@gmail.com>
2025-03-02 03:38:58 +0400

    client-server, задание 1

--- src/main/java/com/my/Server.java
+++ src/main/java/com/my/Server.java
@@ -8,6 +8,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
+/*В серверной и клиентской части сделайте последовательность вызовов в рамках одной сессии таким образом, чтобы получился
+ некий диалог между клиентом и сервером, содержащий от 3 до 5 (или больше) шагов. Например,
+при подключении сервер спрашивает: Write your name. Клиент читает это сообщение и отправляет ему имя.
+Сервер читает имя, запоминает его, и задает следующий вопрос: Are you child? (yes/no). Клиент отвечает yes или no.
+На что сервер, если увидел yes, отвечает Welcome to the kids area, %username%! Let's play!, а если no, то Welcome to the
+adult zone, %username%! Have a good rest, or a good working day! (где %username% - имя клиента из предыдущего шага)*/
+
+
 public class Server {
     public static final Integer PORT = 8084;
 
